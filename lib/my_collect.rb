2780ea0b107a888f025b53dd76551655402ca6f7
idobok <idobok12@gmail.com>
2021-11-12 20:36:54 +0200

    done with confirmation of solution due to weird testing failure

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
+#def my_collect(list)
+    #x = 0
+    #collection = 0
+    #while x < list.length
+    #collection << yield(list[x])
+    #x += 1
+#end
+   # collection
+#end
 
+def my_collect(array)
+    i = 0
+    collect = []
+    while i < array.length
+      collect << yield(array[i])
+      i+=1
+    end
+    collect
+  end
